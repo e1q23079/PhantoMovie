2935c86e6a0452caf102296d0cce03dc3e2cf255
e1q23079 <e1q23079@oit.ac.jp>
2026-07-05 12:35:44 +0900

    app.pyとmain.pyを新規作成し、アプリケーションの基本構造を実装

---
+++ app/app.py
@@ -0,0 +1,12 @@
+def app():
+    """
+    アプリケーション
+    """
+    print("Application is running.")
+
+
+if __name__ == "__main__":
+    """
+    エントリーポイント
+    """
+    app()
---
+++ main.py
@@ -0,0 +1,16 @@
+from app import app
+
+
+def main():
+    """
+    アプリケーションのメイン関数
+    """
+    print("Main function is running.")
+    app.app()
+
+
+if __name__ == "__main__":
+    """
+    エントリーポイント
+    """
+    main()
